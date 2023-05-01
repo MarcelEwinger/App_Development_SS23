067c91ddb9ac8986fdb01bb52ca66ef5bf64b9b1
thetintin123 <96428712+TheTintin123@users.noreply.github.com>
2023-05-01 21:48:29 +0200

    webView finished

--- UE_4/app/src/main/java/com/example/ue4_ewinger_benischke/WebActivity.kt
+++ UE_4/app/src/main/java/com/example/ue4_ewinger_benischke/WebActivity.kt
@@ -1,17 +1,23 @@
 package com.example.ue4_ewinger_benischke
 
+import android.os.Build
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Button
 import android.widget.EditText
 import androidx.activity.ComponentActivity
+import androidx.annotation.RequiresApi
+
 
 class WebActivity : ComponentActivity() {
 
     private lateinit var webView: WebView
     private lateinit var editTextUrl: EditText
     private lateinit var buttonLoad: Button
+    private  var webPrefix = "http://www."
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
@@ -20,10 +26,19 @@ class WebActivity : ComponentActivity() {
         editTextUrl = findViewById(R.id.edit_text_url)
         buttonLoad = findViewById(R.id.button_load)
 
+        webView.webViewClient = WebViewClient()
+
         // Set onClickListener to load webpage
         buttonLoad.setOnClickListener {
-            val url = editTextUrl.text.toString()
+            val url = webPrefix + editTextUrl.text.toString()
             webView.loadUrl(url)
         }
+    }
+
+    override fun onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack()
+        } else
+        super.onBackPressed()
     }
 }
