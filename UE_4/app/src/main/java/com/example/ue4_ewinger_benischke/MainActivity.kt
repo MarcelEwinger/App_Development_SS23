e4edde35fb9bafddefb24f092f9b56694fb4e735
MarcelEwinger <marcel.ewinger@alumni.htl-klu.at>
2023-05-01 12:56:31 +0200

    Implemented Structure

--- UE_4/app/src/main/java/com/example/ue4_ewinger_benischke/MainActivity.kt
+++ UE_4/app/src/main/java/com/example/ue4_ewinger_benischke/MainActivity.kt
@@ -1,21 +1,22 @@
 package com.example.ue4_ewinger_benischke
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-/*
-private const val DEBUG_TAG = "Gestures"
-private lateinit var view: View
-private lateinit var mDetector: GestureDetectorCompat
 
- */
 
 
 class MainActivity : AppCompatActivity(){
 
     private lateinit var singleTouchView: SingleTouchView
     private lateinit var multiTouchView: MultiTouchView
+
+    private lateinit var btnLayout: Button
+    private lateinit var btnConstraint: Button
+    private lateinit var btnWebBrowser: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +25,25 @@ class MainActivity : AppCompatActivity(){
         singleTouchView = findViewById(R.id.singleTouchView)
         multiTouchView = findViewById(R.id.multiTouchView)
 
+        btnLayout = findViewById(R.id.btnLayout)
+        btnConstraint = findViewById(R.id.btnConstraint)
+        btnWebBrowser = findViewById(R.id.btnWebBrowser)
+
+        btnLayout.setOnClickListener {
+            val intent = Intent(this, LayoutActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnConstraint.setOnClickListener {
+            val intent = Intent(this, ConstraintLayoutActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnWebBrowser.setOnClickListener {
+            val intent = Intent(this, PersonalWebBrowserActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 
 }
---
+++ UE_4/app/src/main/java/com/example/ue4_ewinger_benischke/PersonalWebBrowserActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ue4_ewinger_benischke
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PersonalWebBrowserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personal_web_browser)
+    }
+}
