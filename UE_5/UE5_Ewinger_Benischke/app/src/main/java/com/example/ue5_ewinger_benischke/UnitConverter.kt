2ec43378e5da611c255ff6b075967e3dfc4afc5c
thetintin123 <96428712+TheTintin123@users.noreply.github.com>
2023-05-03 21:59:19 +0200

    added timeConverter

--- UE_5/UE5_Ewinger_Benischke/app/src/main/java/com/example/ue5_ewinger_benischke/UnitConverter.kt
+++ UE_5/UE5_Ewinger_Benischke/app/src/main/java/com/example/ue5_ewinger_benischke/UnitConverter.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class UnitConverter : ComponentActivity() {
+
+    lateinit var fromEntry: String
+    lateinit var toEntry: String
+    lateinit var selectedUnit: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +23,9 @@ class UnitConverter : ComponentActivity() {
         val unitSpinner = findViewById<Spinner>(R.id.unit_Spinner)
         val spinnerFrom = findViewById<Spinner>(R.id.spinnerFrom)
         val spinnerTo = findViewById<Spinner>(R.id.spinnerTo)
+        val convertBtn = findViewById<Button>(R.id.convert_button)
+        val inputField = findViewById<EditText>(R.id.input_field)
+        val outputField = findViewById<TextView>(R.id.output_field)
 
         val lengthArray = resources.getStringArray(R.array.unit_length)
         val weightArray = resources.getStringArray(R.array.unit_weight)
@@ -40,7 +50,8 @@ class UnitConverter : ComponentActivity() {
 
         unitSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
-                when (parent.getItemAtPosition(position).toString()) {
+                selectedUnit = parent.getItemAtPosition(position).toString()
+                when (selectedUnit) {
                     "length" -> {spinnerFrom.adapter = lengthAdapter
                                 spinnerTo.adapter = lengthAdapter}
                     "weight" -> {spinnerFrom.adapter = weightAdapter
@@ -56,10 +67,65 @@ class UnitConverter : ComponentActivity() {
 
             override fun onNothingSelected(parent: AdapterView<*>) {
                 // Do nothing
-                println("Nothing selected")
+            }
+        }
+
+        spinnerFrom.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                fromEntry = parent.getItemAtPosition(position).toString()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Do nothing
+            }
+        }
+
+        spinnerTo.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                toEntry = parent.getItemAtPosition(position).toString()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Do nothing
+            }
+        }
+
+        convertBtn.setOnClickListener {
+            val inputValue = inputField.text.toString().toDouble()
+            if (fromEntry == toEntry)
+                outputField.text = inputValue.toString()
+            else {
+                var outputValue = 0.0
+                when (selectedUnit) {
+                    "time" -> outputValue = convertTime(inputValue)
+                }
+                outputField.text = outputValue.toString()
             }
         }
     }
 
+    private fun convertTime(time: Double):Double{
+        var value = 0.0
+        when (fromEntry) {
+            "hour" ->
+               when (toEntry) {
+                    "minute" -> value = time * 60
+                    "second" -> value = time * 3600
+                }
+
+            "minute" ->
+                when (toEntry) {
+                    "hour" -> value = time / 60
+                    "second" -> value = time * 60
+                }
+
+            "second" ->
+                when (toEntry) {
+                    "minute" -> value = time / 60
+                    "hour" -> value = time / 3600
+                }
+        }
+        return value
+    }
 
 }
