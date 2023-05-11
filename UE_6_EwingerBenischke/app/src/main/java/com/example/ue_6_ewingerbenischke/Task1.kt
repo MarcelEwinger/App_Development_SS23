f5248b5029650cd4fe502f5eaf91abf4b9753660
MarcelEwinger <marcel.ewinger@alumni.htl-klu.at>
2023-05-11 12:35:17 +0200

    added Logic

--- UE_6_EwingerBenischke/app/src/main/java/com/example/ue_6_ewingerbenischke/Task1.kt
+++ UE_6_EwingerBenischke/app/src/main/java/com/example/ue_6_ewingerbenischke/Task1.kt
@@ -7,25 +7,88 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Build
 import android.content.Context
+import android.content.ActivityNotFoundException
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+
+import android.view.View
+import android.widget.ImageButton
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+import androidx.activity.ComponentActivity
 
 
 class Task1 : AppCompatActivity() {
+    private val TAG = "LAYOUT"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val isTV = isTVDevice(this)
         val isWidescreen = isWidescreenDevice(this)
         val isSmall = isSmallDevice(this)
 
+
         // Das richtige Layout basierend auf der Bildschirmgröße auswählen
         if (isWidescreen) {
-            setContentView(R.layout.widescreen_layout) // Widescreen-Layout
+            setContentView(R.layout.activity_task1) // Widescreen-Layout
+            Log.d(TAG, "Widescreen-Layout")
         } else if (isTV) {
             setContentView(R.layout.television_layout) // TV-Layout
+            Log.d(TAG, "TV-Layout")
         }else if(isSmall){
             setContentView(R.layout.small_layout) // Small-Layout
+            Log.d(TAG, "Small-Layout")
         }else {
             setContentView(R.layout.activity_task1) // Normal-Layout
+            Log.d(TAG, "Normal-Layout")
         }
+
+        // Aktiviere den Vollbildmodus
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+
+
+        val exitButton = findViewById<ImageButton>(R.id.btnExit)
+        exitButton.setOnClickListener {
+            finish()
+        }
+
+        val facebookButton = findViewById<ImageButton>(R.id.facebook_icon)
+        facebookButton.setOnClickListener {
+            val url = "https://www.facebook.com/uniklagenfurt/"
+            val facebookIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(facebookIntent)
+        }
+        val twitterButton = findViewById<ImageButton>(R.id.twitter_icon)
+        twitterButton.setOnClickListener {
+            val url = "https://twitter.com/alpenadriauni?lang=de"
+            val twitterIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(twitterIntent)
+        }
+        val instagramButton = findViewById<ImageButton>(R.id.instagram_icon)
+        instagramButton.setOnClickListener {
+            val url = "https://www.instagram.com/uniklagenfurt/?hl=de"
+            val instagramIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(instagramIntent)
+        }
+        val mailButton = findViewById<ImageButton>(R.id.email_icon)
+        mailButton.setOnClickListener {
+            try {
+                val mailIntent = Intent(Intent.ACTION_SENDTO)
+                mailIntent.data = Uri.parse("mailto:") // only email apps should handle this
+                mailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("kobenischke@edu.aau.at"))
+                mailIntent.putExtra(Intent.EXTRA_SUBJECT, "App feedback")
+                startActivity(mailIntent)
+            } catch (ex: ActivityNotFoundException) {
+                Toast.makeText(this,
+                    "There are no email client installed on your device.",LENGTH_SHORT
+                ).show()
+            }
+        }
+
+
 
 
 
