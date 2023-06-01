b3e1c4cf2e922fa308317e3e563575b32c225a58
MarcelEwinger <marcel.ewinger@alumni.htl-klu.at>
2023-06-01 14:18:22 +0200

    Implemented Task1

--- UE8/app/src/main/java/com/example/ue8_ewinger_benischke/Task1.kt
+++ UE8/app/src/main/java/com/example/ue8_ewinger_benischke/Task1.kt
@@ -1,11 +1,42 @@
 package com.example.ue8_ewinger_benischke
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.RadioGroup
+import androidx.appcompat.app.AppCompatDelegate
 
 class Task1 : AppCompatActivity() {
+    private lateinit var radioGroup: RadioGroup
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Set the initial theme based on the "isDarkTheme" flag
+        //setTheme(R.style.Theme_UE8_Ewinger_Benischke)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task1)
+        radioGroup = findViewById(R.id.idRGgroup)
+
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                R.id.idRBLight -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+                R.id.idRBDark -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                }
+            }
+        }
+
+
+
+
+
+
+
+
     }
+
 }
