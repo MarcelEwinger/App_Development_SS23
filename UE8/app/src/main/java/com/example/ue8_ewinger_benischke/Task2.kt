e17d1729115a2a8ef8cf020ece453331653b2269
MarcelEwinger <marcel.ewinger@alumni.htl-klu.at>
2023-06-03 17:24:10 +0200

    Implemented Task4

--- UE8/app/src/main/java/com/example/ue8_ewinger_benischke/Task2.kt
+++ UE8/app/src/main/java/com/example/ue8_ewinger_benischke/Task2.kt
@@ -1,11 +1,25 @@
 package com.example.ue8_ewinger_benischke
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
 
 class Task2 : AppCompatActivity() {
+    private lateinit var motionLayout: MotionLayout
+    private lateinit var button: Button
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task2)
+
+
+
     }
+
+
+
 }
--- UE8/app/src/main/java/com/example/ue8_ewinger_benischke/Task4.kt
+++ UE8/app/src/main/java/com/example/ue8_ewinger_benischke/Task4.kt
@@ -2,10 +2,39 @@ package com.example.ue8_ewinger_benischke
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TimePicker
+import android.widget.Toast
+import androidx.constraintlayout.motion.widget.MotionLayout
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
 class Task4 : AppCompatActivity() {
+    private lateinit var readValues: Button
+    private lateinit var date: DatePicker
+    private lateinit var time: TimePicker
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task4)
+
+        readValues = findViewById(R.id.btn)
+        date = findViewById(R.id.datepicker)
+        time = findViewById(R.id.timepicker)
+        time.setIs24HourView(true);
+
+        val calendar: Calendar = Calendar.getInstance()
+
+
+        readValues.setOnClickListener(){
+            calendar.set(date.year, date.month, date.dayOfMonth)
+            val simpleDateFormat = SimpleDateFormat("dd.MM.yyyy")
+            val formatted: String = simpleDateFormat.format(calendar.time)
+            val hour = time.hour
+            val min = time.minute
+            Toast.makeText(this, "$formatted um $hour:$min", Toast.LENGTH_SHORT).show();
+        }
     }
 }
